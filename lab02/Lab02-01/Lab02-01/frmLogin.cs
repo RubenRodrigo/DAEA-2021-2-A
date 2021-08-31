7b1c681f39560c0464eac754159094d3f543bc02
Ruben Rodrigo <ruben.chani@tecsup.edu.pe>
2021-08-30 22:10:10 -0500

    Lab02 Parte - 3: Funcionalidad basica del Login

--- lab02/Lab02-01/Lab02-01/frmLogin.cs
+++ lab02/Lab02-01/Lab02-01/frmLogin.cs
@@ -19,9 +19,21 @@ namespace Lab02_01
 
         private void btnIniciar_Click(object sender, EventArgs e)
         {
-            PrincipalMDI principal = new PrincipalMDI();
-            principal.Show();
-            this.Hide();
+            string user = "admin";
+            string pass = "hola mundo";
+
+            string userTxt = txtUsuario.Text;
+            string passTxt = txtPassword.Text;
+            Console.WriteLine(userTxt);
+            Console.WriteLine(passTxt);
+            if (userTxt.Equals(user) && passTxt.Equals(pass))
+            {
+                Console.WriteLine("Coinciden");
+                PrincipalMDI principal = new PrincipalMDI();
+                principal.Show();
+                this.Hide();
+            }
+
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
